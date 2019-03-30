9be7a4d573e05de7e806c2cbc5cbdc7632e286e2
t o <finalfantasytimothy@gmail.com>
2019-03-30 13:13:03 -0700

    Added the basic Hash

--- src/spellCheck.h
+++ src/spellCheck.h
@@ -9,9 +9,25 @@
  ************************************************************************/
 #include <iostream>
 #include <string>
+#include "hash.h"
+
 
 #ifndef SPELL_CHECK_H
 #define SPELL_CHECK_H
+template <class T>
+class sHash: public Hash<T>
+{
+public: 
+	int hash(const std::string & item)
+	{		      
+		int sum = 0;
+		for (int k = 0; k < item.length(); k++)
+			sum = sum + int(item[k]);
+		return  sum % Hash<T>::size();
+	}
+
+};
+
 
 void spellCheck()
 {
